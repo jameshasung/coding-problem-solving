cbd6a232a6a622ad5c72197857b0120032b952fb
jameshasung <117165485+jameshasung@users.noreply.github.com>
2023-03-10 13:08:31 +0900

    230310 점심먹고

---
+++ 1193.py
@@ -0,0 +1,17 @@
+import sys
+
+X = int(sys.stdin.readline().strip())
+
+line = 1
+while X > line:
+    X -= line
+    line += 1
+
+if line % 2 == 0:
+    a = X
+    b = line-X+1
+else:
+    a = line-X+1
+    b = X
+
+print(a, '/', b, sep='')
