37f0594b8de40c7080ec8703cc0484a8a98c2dd6
jameshasung <117165485+jameshasung@users.noreply.github.com>
2023-03-26 19:43:05 +0900

    230326 구현문제

---
+++ 이것이코딩테스트다/4_1.py
@@ -0,0 +1,25 @@
+# 예제 4-1 상하좌우
+# 제한시간 15분
+import sys
+
+n = int(sys.stdin.readline())
+map = [[0] * n for _ in range(n)]
+
+cmd = list(sys.stdin.readline().split())
+
+x, y = 1, 1
+for i in range(len(cmd)):
+    if (x == 1 and cmd[i] == 'L'):
+        continue
+    elif (y == 1 and cmd[i] == 'U'):
+        continue
+    elif (cmd[i] == 'R'):
+        x += 1
+    elif (cmd[i] == 'L'):
+        x -= 1
+    elif (cmd[i] == 'U'):
+        y -= 1
+    elif (cmd[i] == 'D'):
+        y += 1
+
+print(x, y)
